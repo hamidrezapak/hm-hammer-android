a46fa02a65bf34ddf4b7c7e7556f663648b5b6c1
hamidrezapak <hamidrezapak@gmail.com>
2026-09-12 06:33:14 +0330

    fix: remove duplicate @HiltViewModel annotation left over from earlier constructor edit

--- app/src/main/java/com/example/ui/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/ui/viewmodel/MainViewModel.kt
@@ -64,7 +64,6 @@ data class AuditLog(
 )
 
 @HiltViewModel
-@HiltViewModel
 class MainViewModel @Inject constructor(
     private val placeOrderUseCase: PlaceOrderUseCase,
     private val fetchBalanceUseCase: FetchBalanceUseCase
